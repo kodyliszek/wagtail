28dafaec8b80abcc3f8e464ebf8c70520910da90
dfilipkowski <damian.filipkowski@idego.io>
2022-08-04 14:56:50 +0200

    DATABASES config added to base.py

--- esgia/settings/base.py
+++ esgia/settings/base.py
@@ -84,22 +84,6 @@ WSGI_APPLICATION = "esgia.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#     }
-# }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DBNAME'],
-        'HOST': os.environ['DBHOST'],
-        'USER': os.environ['DBUSER'],
-        'PASSWORD': os.environ['DBPASSWORD']
-    }
-}
 
 
 # Password validation
--- esgia/settings/production.py
+++ esgia/settings/production.py
@@ -6,3 +6,13 @@ try:
     from .local import *
 except ImportError:
     pass
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DBNAME'],
+        'HOST': os.environ['DBHOST'],
+        'USER': os.environ['DBUSER'],
+        'PASSWORD': os.environ['DBPASSWORD']
+    }
+}
